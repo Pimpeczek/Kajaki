64cf41eefccf08fbd4e3a9ef30749b6f3428c149
BuildTools <unconfigured@null.spigotmc.org>
2019-06-01 18:28:07 +0200

    Add project files.

---
+++ Kajaki/Misc.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Kajaki
+{
+    class Misc
+    {
+        public static int CountChars(string str, char ch)
+        {
+            int count = 0;
+            foreach (char c in str)
+                if (c == ch) count++;
+            return count;
+        }
+
+        public static string GetFilledString(int len, char c)
+        {
+            string str = "";
+            for (int i = 0; i < len; i++)
+                str += c;
+            return str;
+        }
+    }
+
+    class Arit
+    {
+        public static int Clamp(int value, int inclusiveMin, int inclusiveMax)
+        {
+            if (value < inclusiveMin)
+                return inclusiveMin;
+            if (value > inclusiveMax)
+                return inclusiveMax;
+            return value;
+        }
+
+        public static int CountTrues(bool[] boolArr)
+        {
+            int counter = 0;
+            for (int i = 0; i < boolArr.Length; i++)
+                if (boolArr[i])
+                    counter++;
+            return counter;
+        }
+
+        public static bool[] NegateBoolArray(bool[] boolArr)
+        {
+            for (int i = 0; i < boolArr.Length; i++)
+                boolArr[i] = !boolArr[i];
+            return boolArr;
+        }
+
+        public static bool[] GetBoolArray(int lenght, bool state)
+        {
+            bool[] boolArr = new bool[lenght];
+            for (int i = 0; i < boolArr.Length; i++)
+                boolArr[i] = state;
+            return boolArr;
+        }
+
+        public static int[] GetIntArray(int lenght, int state)
+        {
+            int[] boolArr = new int[lenght];
+            for (int i = 0; i < boolArr.Length; i++)
+                boolArr[i] = state;
+            return boolArr;
+        }
+
+        public static int TakeGreater(int x, int y)
+        {
+            return (x > y ? x : y);
+        }
+
+        public static int TakeLower(int x, int y)
+        {
+            return (x < y ? x : y);
+        }
+    }
+
+    class Rand
+    {
+        static Random rng = new Random();
+
+        public static void SetSeed(int seed)
+        {
+            rng = new Random(seed);
+        }
+
+        public static int Int()
+        {
+            return rng.Next();
+        }
+        public static int Int(int exclusiveMax)
+        {
+            return rng.Next(exclusiveMax);
+        }
+        public static int Int(int inclusiveMin, int exclusiveMax)
+        {
+            return rng.Next(inclusiveMin, exclusiveMax);
+        }
+
+    }
+
+    class MyColor
+    {
+        public static Color NegateColor(Color c)
+        {
+            return Color.FromArgb(255 - c.R, 255 - c.G, 255 - c.B);
+        }
+        public static Color MultplyColor(Color c, float multi)
+        {
+            int iMulti = (int)(multi * 255);
+            return Color.FromArgb(c.R * iMulti / 255, c.G * iMulti / 255, c.B * iMulti / 255);
+        }
+        public static Color RandomColor()
+        {
+            return Color.FromArgb(Rand.Int(256), Rand.Int(256), Rand.Int(256));
+        }
+        public static Color RandomColor(int seed)
+        {
+            Random rng = new Random(seed);
+            return Color.FromArgb(rng.Next(256), rng.Next(256), rng.Next(256));
+        }
+    }
+
+    public class Int2
+    {
+        public int x, y;
+        public static Int2 zero = new Int2(0, 0);
+        public static Int2 one = new Int2(1, 1);
+
+        public Int2()
+        {
+            x = 0;
+            y = 0;
+        }
+        public Int2(int _x, int _y)
+        {
+            x = _x; y = _y;
+        }
+
+        override public string ToString()
+        {
+            return x + ", " + y;
+        }
+
+        public static Int2 Random()
+        {
+            return new Int2(Rand.Int(), Rand.Int());
+        }
+        public static Int2 Random(int exclusiveMax)
+        {
+            return new Int2(Rand.Int(exclusiveMax), Rand.Int(exclusiveMax));
+        }
+        public static Int2 Random(Int2 exclusiveMax)
+        {
+            return new Int2(Rand.Int(exclusiveMax.x), Rand.Int(exclusiveMax.y));
+        }
+        public static Int2 Random(int exclusiveMin, int exclusiveMax)
+        {
+            return new Int2(Rand.Int(exclusiveMin, exclusiveMax), Rand.Int(exclusiveMin, exclusiveMax));
+        }
+        public static Int2 Random(Int2 exclusiveMin, Int2 exclusiveMax)
+        {
+            return new Int2(Rand.Int(exclusiveMin.x, exclusiveMax.y), Rand.Int(exclusiveMin.y, exclusiveMax.y));
+        }
+
+        public static bool InBox(Int2 aSize, Int2 coords)
+        {
+            return coords <= aSize && coords >= zero;
+        }
+        public static bool InBox(Int2 aSize, Int2 aCoords, Int2 bCoords)
+        {
+            return bCoords <= aSize + aCoords && bCoords >= aCoords;
+        }
+        public static bool InBoxInBox(Int2 aSize, Int2 aCoords, Int2 bSize, Int2 bCoords)
+        {
+            return bCoords + bSize <= aSize + aCoords && bCoords >= aCoords;
+        }
+
+
+        //Definicje operatorów
+        public static Int2 operator +(Int2 i1, Int2 i2) { return new Int2(i1.x + i2.x, i1.y + i2.y); }
+        public static Int2 operator +(Int2 i1, int x) { return new Int2(i1.x + x, i1.y + x); }
+
+        public static Int2 operator -(Int2 i1, Int2 i2) { return new Int2(i1.x - i2.x, i1.y - i2.y); }
+        public static Int2 operator -(Int2 i1, int x) { return new Int2(i1.x - x, i1.y - x); }
+
+        public static Int2 operator *(Int2 i1, Int2 i2) { return new Int2(i1.x * i2.x, i1.y * i2.y); }
+        public static Int2 operator *(Int2 i1, int x) { return new Int2(i1.x * x, i1.y * x); }
+
+        public static Int2 operator /(Int2 i1, Int2 i2) { return new Int2(i1.x / i2.x, i1.y / i2.y); }
+        public static Int2 operator /(Int2 i1, int x) { return new Int2(i1.x / x, i1.y / x); }
+
+        public static bool operator ==(Int2 i1, Int2 i2) { if (i1.x == i2.x && i1.y == i2.y) return true; return false; }
+        public static bool operator !=(Int2 i1, Int2 i2) { if (i1.x == i2.x && i1.y == i2.y) return false; return true; }
+        public static bool operator >=(Int2 i1, Int2 i2) { if (i1.x >= i2.x && i1.y >= i2.y) return true; return false; }
+        public static bool operator <=(Int2 i1, Int2 i2) { if (i1.x <= i2.x && i1.y <= i2.y) return true; return false; }
+        public static bool operator >(Int2 i1, Int2 i2) { if (i1.x > i2.x && i1.y > i2.y) return true; return false; }
+        public static bool operator <(Int2 i1, Int2 i2) { if (i1.x < i2.x && i1.y < i2.y) return true; return false; }
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (this.GetType() != obj.GetType()) return false;
+            Int2 p = (Int2)obj;
+            return this == p;
+        }
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var result = 0;
+                result = (result * 397) ^ x;
+                result = (result * 397) ^ y;
+                return result;
+            }
+        }
+
+
+
+    }
+
+    class Boxes
+    {
+        public enum BoxType { light, round, normal, doubled, dashed, dashedLight};
+        public static readonly Char cor_b_lu = '╔';
+        public static readonly Char cor_b_ru = '╗';
+        public static readonly Char cor_b_rl = '╝';
+        public static readonly Char cor_b_ll = '╚';
+        public static readonly Char cor_b_notd = '╩';
+        public static readonly Char cor_b_notu = '╦';
+        public static readonly Char cor_b_notl = '╠';
+        public static readonly Char cor_b_notr = '╣';
+        public static readonly Char cor_b_cross = '╬';
+        public static readonly Char wal_b_h = '║';
+        public static readonly Char wal_b_v = '═';
+
+        public static readonly Char cor_lu = '┏';
+        public static readonly Char cor_ru = '┓';
+        public static readonly Char cor_rl = '┛';
+        public static readonly Char cor_ll = '┗';
+        public static readonly Char cor_notd = '┻';
+        public static readonly Char cor_notu = '┳';
+        public static readonly Char cor_notl = '┣';
+        public static readonly Char cor_notr = '┫';
+        public static readonly Char cor_cross = '╋';
+        public static readonly Char wal_h = '┃';
+        public static readonly Char wal_v = '━';
+
+        public static readonly Char[] doubleBoxChars =      { '╗', ' ', '═', '╔', '╦', ' ', '╝', '║', '╣', '╚', '╩', '╠', '╬' };
+        public static readonly Char[] boxChars =            { '┓', ' ', '━', '┏', '┳', ' ', '┛', '┃', '┫', '┗', '┻', '┣', '╋' };
+        public static readonly Char[] lightBoxChars =       { '┐', ' ', '─', '┌', '┬', ' ', '┘', '│', '┤', '└', '┴', '├', '┼' };
+        public static readonly Char[] roundBoxChars =       { '╮', ' ', '─', '╭', '┬', ' ', '╯', '│', '┤', '╰', '┴', '├', '┼' };
+        public static readonly Char[] dashedLightBoxChars = { '┐', ' ', '┄', '┌', '┬', ' ', '┘', '┊', '┤', '└', '┴', '├', '┼' };
+        public static readonly Char[] dashedBoxChars =      { '┓', ' ', '┅', '┏', '┳', ' ', '┛', '┋', '┫', '┗', '┻', '┣', '╋' };
+        public static Char GetBoxChar(int id)
+        {
+            id -= 3;
+            if (id < 0 || id > 12)
+                return ' ';
+            return boxChars[id];
+        }
+        public static Char GetDoubleBoxChar(int id)
+        {
+            id -= 3;
+            if (id < 0 || id > 12)
+                return ' ';
+            return doubleBoxChars[id];
+        }
+        public static Char GetLightBoxChar(int id)
+        {
+            id -= 3;
+            if (id < 0 || id > 12)
+                return ' ';
+            return lightBoxChars[id];
+        }
+        public static Char GetDashedBoxChar(int id)
+        {
+            id -= 3;
+            if (id < 0 || id > 12)
+                return ' ';
+            return dashedBoxChars[id];
+        }
+
+        public static void DrawBox(BoxType boxType, int x, int y, int sx, int sy)
+        {
+            Char[] bs;
+            switch(boxType)
+            {
+                case BoxType.dashed:
+                    bs = dashedBoxChars;
+                    break;
+                case BoxType.dashedLight:
+                    bs = dashedLightBoxChars;
+                    break;
+                case BoxType.doubled:
+                    bs = doubleBoxChars;
+                    break;
+                case BoxType.light:
+                    bs = lightBoxChars;
+                    break;
+                case BoxType.round:
+                    bs = roundBoxChars;
+                    break;
+                default:
+                    bs = boxChars;
+                    break;
+            }
+            Console.SetCursorPosition(x, y);
+            Console.Write($"{bs[3]}{"".PadLeft(sx - 2, bs[2])}{bs[0]}");
+            string midBorder = $"{bs[7]}{"".PadLeft(sx - 2)}{bs[7]}";
+            for (int i = 1; i < sy - 1; i++)
+            {
+                Console.SetCursorPosition(x, y + i);
+                Console.Write(midBorder);
+            }
+            Console.SetCursorPosition(x, y + sy - 1);
+            Console.Write($"{bs[9]}{"".PadLeft(sx - 2, bs[2])}{bs[6]}");
+        }
+    }
+
+    class Bars
+    {
+        public static readonly Char b_0 = '▏';
+        public static readonly Char b_1 = '▎';
+        public static readonly Char b_2 = '▍';
+        public static readonly Char b_3 = '▌';
+        public static readonly Char b_4 = '▋';
+        public static readonly Char b_5 = '▊';
+        public static readonly Char b_6 = '▉';
+        public static readonly Char b_7 = '█';
+        public static readonly Char[] bars = { '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█' };
+        public static readonly Char bv_0 = '▁';
+        public static readonly Char bv_1 = '▂';
+        public static readonly Char bv_2 = '▃';
+        public static readonly Char bv_3 = '▄';
+        public static readonly Char bv_4 = '▅';
+        public static readonly Char bv_5 = '▆';
+        public static readonly Char bv_6 = '▇';
+        public static readonly Char bv_7 = '█';
+        public static readonly Char[] barsVertical = { '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█' };
+        public static readonly Char t0 = ' ';
+        public static readonly Char t1 = '░';
+        public static readonly Char t2 = '▒';
+        public static readonly Char t3 = '▓';
+        public static readonly Char t4 = '█';
+        public static readonly Char[] transparent = { ' ', '░', '▒', '▓', '█'};
+
+
+        public static Char GetBarSegment(int id)
+        {
+            if (id < 0 || id > 7)
+            {
+                return ' ';
+            }
+            return bars[id];
+        }
+        public static Char GetVerticalBarSegment(int id)
+        {
+            if (id < 0 || id > 7)
+            {
+                return ' ';
+            }
+            return barsVertical[id];
+        }
+    }
+}
---
+++ Kajaki/Program.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Drawing;
+using Pastel;
+
+namespace Kajaki
+{
+    class Program
+    {
+        static bool clearOnExit;
+        static bool redrawBoxes;
+        static void Main(string[] args)
+        {
+            clearOnExit = true;
+            Console.SetWindowSize(40, 20);
+            Menu mainMenu = new Menu(new Int2(0,0), new Int2(40, 4), "Main menu", Boxes.BoxType.doubled);
+            mainMenu.AddOption("Host a game", 0);
+            mainMenu.AddOption("Join a game", 1);
+            mainMenu.AddOption("Ship designer", 2);
+            mainMenu.AddOption("Exit", -1);
+            int response;
+            do
+            {
+                response = mainMenu.WaitForInput();
+            } while (response != -1);
+
+        }
+
+    }
+    class Renderer
+    {
+        public void Write(string text, Int2 position)
+        {
+            Write(text, position.x, position.y);
+        }
+        public void Write(string text, int x, int y)
+        {
+
+        }
+    }
+
+    class Menu
+    {
+        public Int2 Position { get; protected set; }
+        public Int2 Size { get; protected set; }
+        public string Name { get; protected set; }
+        Boxes.BoxType boxType;
+        List<(string, int)> options;
+        int hPoint;
+        int scrollPoint;
+        public Menu(Int2 position, Int2 size, string name, Boxes.BoxType boxType)
+        {
+            options = new List<(string, int)>();
+            hPoint = 0;
+            scrollPoint = 0;
+            Position = position;
+            Size = size;
+            Name = name;
+            this.boxType = boxType;
+        }
+
+        public void AddOption(string text, int id)
+        {
+            options.Add((text, id));
+        }
+
+        public int WaitForInput()
+        {
+            ConsoleKey response;
+            Draw();
+            DrawOptions();
+            do
+            {
+                response = Console.ReadKey(true).Key;
+                switch (response)
+                {
+                    case ConsoleKey.UpArrow:
+                        hPoint--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        hPoint++;
+                        break;
+                }
+                if( hPoint < 0)
+                    hPoint += (options != null && options.Count > 0 ? options.Count : 1);
+                hPoint %= (options != null && options.Count > 0 ? options.Count : 1);
+                
+                
+                DrawOptions();
+
+            } while (response != ConsoleKey.Enter);
+
+            return options[hPoint].Item2;
+        }
+        
+
+        public void Draw()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Boxes.DrawBox(boxType, Position.x, Position.y, Size.x, Size.y);
+            Console.SetCursorPosition(Position.x + (Size.x - Name.Length) / 2, Position.y);
+            Console.Write(Name);
+            
+        }
+
+
+        void DrawOptions()
+        {
+            int startHeight = Arit.Clamp((Size.y - 2 - options.Count) / 2, 0, Size.y);
+            if(hPoint >= Size.y - 2)
+            {
+                scrollPoint = Arit.Clamp(Size.y - 2 - hPoint, 0, options.Count - Size.y + 2);
+            }
+            Console.SetCursorPosition(45, 0);
+            Console.Write(scrollPoint);
+            Console.SetCursorPosition(45, 1);
+            Console.Write(hPoint);
+            for (int i = 0; i < options.Count && i < Size.y - 2; i++)
+            {
+                Console.SetCursorPosition(Position.x + (Size.x - options[i + scrollPoint].Item1.Length) / 2, Position.y + startHeight + i - scrollPoint+ 1);
+                if (hPoint - scrollPoint == i)
+                {
+                    Console.Write(options[i + scrollPoint].Item1.PastelBg(Color.Gray));
+                }
+                else
+                {
+                    Console.Write(options[i + scrollPoint].Item1);
+                }
+
+            }
+        }
+    }
+
+
+    class Map
+    {
+        public Int2 Position { get; protected set; }
+        public Int2 Size { get; protected set; }
+        List<Ship> ships;
+
+        Int2 contentPosition;
+        Int2 contentSize;
+        string waterLane;
+        string upBorder;
+        string midBorder;
+        string downBorder;
+        int frame;
+
+
+        public Map(Int2 size, List<Ship> ships)
+        {
+            this.ships = ships;
+            Position = new Int2(2, 2);
+            contentPosition = new Int2(Position.x + 1, Position.y + 1);
+            contentSize = new Int2(size.x - 2, size.y - 2);
+            Size = size;
+            frame = 0;
+
+            waterLane = Misc.GetFilledString(size.x - 2, Bars.transparent[1]);
+            upBorder = $"█{Misc.GetFilledString(size.x - 2, '█')}█";
+            midBorder = $"█{waterLane}█";
+            downBorder = $"█{Misc.GetFilledString(size.x - 2, '█')}█";
+        }
+
+
+        public void Draw()
+        {
+            DrawMapRaw();
+            Ship tShip;
+            string shipLine;
+            for(int i = 0; i < ships.Count; i++)
+            {
+                tShip = ships[i];
+                if (tShip.Changed)
+                {
+                    for (int y = 0; y < tShip.Size.y; y++)
+                    {
+                        shipLine = "";
+                        for (int x = 0; x < tShip.Size.x; x++)
+                        {
+                            shipLine += (tShip.Decks[x, y] == null ? waterLane[x] : tShip.Decks[x, y].symbol);
+                        }
+                        Console.SetCursorPosition(contentPosition.x + tShip.Position.x, contentPosition.y + tShip.Position.y + y);
+                        Console.Write(shipLine);
+                    }
+                }
+            }
+        }
+
+        void DrawMapRaw()
+        {
+            Console.SetCursorPosition(Position.x, Position.y);
+            Console.Write(upBorder);
+            Console.SetCursorPosition(Position.x, Position.y+ Size.y -1);
+            Console.Write(downBorder);
+            for (int y = 1; y < Size.y - 1; y++)
+            {
+                Console.SetCursorPosition(Position.x, Position.y + y);
+                Console.Write(midBorder);
+            }
+        }
+
+
+    }
+
+    class Deck
+    {
+        public char symbol;
+        public Deck(char symbol)
+        {
+            this.symbol = symbol;
+        }
+    }
+
+    class Ship
+    {
+        public Int2 Position { get; protected set; }
+        public Int2 Size { get; protected set; }
+        public Deck[,] Decks { get; protected set; }
+        public bool Changed { get; protected set; }
+        Deck[,] oryginalDecks;
+        Int2 oryginalSize;
+        int dir; // 0 = right 1 = down ...
+        public Ship(Int2 position, Deck[,] decks)
+        {
+            Random rng = new Random();
+            Position = position;
+            Changed = true;
+            decks = new Deck[5, 8];
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 8; j++)
+                {
+                    if (rng.Next(2) == 0)
+                        decks[i, j] = new Deck('#');
+                }
+            oryginalSize = new Int2(decks.GetLength(0), decks.GetLength(1));
+            Size = oryginalSize;
+            oryginalDecks = decks;
+            this.Decks = decks;
+            dir = 0;
+        }
+
+        public bool Rotate(int right)
+        {
+            dir += right;
+            dir %= 4;
+            return true;
+        }
+
+        public bool RotateRight()
+        {
+            oryginalDecks = Decks;
+            oryginalSize = Size;
+            Decks = new Deck[Size.y, Size.x];
+            for (int x = 0; x < Size.x; x++)
+            {
+                for (int y = 0; y < Size.y; y++)
+                {
+                    Decks[Size.y - y - 1, x] = oryginalDecks[x, y];
+                }
+            }
+            Size = new Int2(oryginalSize.y, oryginalSize.x);
+            return true;
+        }
+
+        public bool RotateLeft()
+        {
+            oryginalDecks = Decks;
+
+            Decks = new Deck[Size.y, Size.x];
+
+            for (int x = 0; x < Size.x; x++)
+            {
+                for (int y = 0; y < Size.y; y++)
+                {
+                    Decks[y, Size.x - x - 1] = oryginalDecks[x, y];
+                }
+            }
+            Size = new Int2(oryginalSize.y, oryginalSize.x);
+            return true;
+        }
+
+        public void Draw()
+        {
+            string line;
+            for (int y = 0; y < Size.y; y++)
+            {
+                line = "";
+                for (int x = 0; x < Size.x; x++)
+                {
+                    line += (Decks[x, y] == null ? ' ' : '#');
+                }
+                Console.SetCursorPosition(2, 2 + y);
+                Console.Write(line);
+            }
+
+        }
+
+    }
+
+
+}
